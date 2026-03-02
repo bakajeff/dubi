391edea463b3d77c52bd68837aa881abc5278b7f
jeffte <32723610+bakajeff@users.noreply.github.com>
2026-03-02 15:58:53 -0300

    feat: add remove option buttons

--- lib/dubi_web/live/poll_live/new.ex
+++ lib/dubi_web/live/poll_live/new.ex
@@ -16,6 +16,9 @@ defmodule DubiWeb.PollLive.New do
   end
 
   def handle_event("validate", %{"poll" => poll_params}, socket) do
+    # Make sure Ecto doesn't see empty IDs as duplicates
+    poll_params = scrub_options(poll_params)
+
     changeset =
       %Poll{}
       |> Voting.change_poll(poll_params)
@@ -25,15 +28,32 @@ defmodule DubiWeb.PollLive.New do
   end
 
   def handle_event("add-option", _, socket) do
-    # Get current params from the form
-    params = socket.assigns.form.params
-    # Add a new blank option to the list
-    options = Map.get(params, "options", %{})
-    next_index = Enum.count(options) |> Integer.to_string()
-    new_options = Map.put(options, next_index, %{"label" => ""})
+    # Get current options from the form's changeset
+    changeset = socket.assigns.form.source
+    existing_options = Ecto.Changeset.get_assoc(changeset, :options)
 
-    params = Map.put(params, "options", new_options)
-    changeset = Voting.change_poll(%Poll{}, params)
+    # Add a new blank option
+    new_options = existing_options ++ [%Option{}]
+
+    # Update the changeset
+    changeset =
+      changeset
+      |> Ecto.Changeset.put_assoc(:options, new_options)
+
+    {:noreply, assign(socket, form: to_form(changeset))}
+  end
+
+  def handle_event("remove-option", %{"index" => index}, socket) do
+    index = String.to_integer(index)
+    changeset = socket.assigns.form.source
+
+    existing_options = Ecto.Changeset.get_assoc(changeset, :options)
+    new_options = List.delete_at(existing_options, index)
+
+    changeset =
+      changeset
+      |> Ecto.Changeset.put_assoc(:options, new_options)
+      |> Map.put(:action, :validate)
 
     {:noreply, assign(socket, form: to_form(changeset))}
   end
@@ -48,37 +68,62 @@ defmodule DubiWeb.PollLive.New do
     end
   end
 
+  defp scrub_options(params) do
+    if options = params["options"] do
+      scrubbed =
+        Enum.into(options, %{}, fn {k, v} ->
+          {k, Map.delete(v, "id")}
+        end)
+
+      Map.put(params, "options", scrubbed)
+    else
+      params
+    end
+  end
+
   def render(assigns) do
     ~H"""
-    <div class="">
-      <h1 class="">Create a New Poll</h1>
+    <div class="container max-w-2xl mx-auto bg-base-200 p-8 rounded-lg">
+      <h1 class="">Question & Answers</h1>
 
       <.form for={@form} phx-change="validate" phx-submit="save" class="space-y-4">
         <div>
-          <.input field={@form[:question]} label="Question" placeholder="What's on your mind?" />
+          <.input field={@form[:question]} label="Ask a question" placeholder="What's on your mind?" />
         </div>
 
         <div class="">
           <label class="">Options</label>
           <.inputs_for :let={option} field={@form[:options]}>
-            <div class="">
-              <.input field={option[:label]} placeholder={"Option #{option.index + 1}"} />
+            <div class="flex gap-2 items-center">
+              <div class="grow">
+                <.input field={option[:label]} placeholder={"Option #{option.index + 1}"} />
+              </div>
+
+              <button
+                type="button"
+                phx-click="remove-option"
+                phx-value-index={option.index}
+                class="text-red-500 hover:text-red-700"
+              >
+                <.icon name="hero-minus-mini" />
+              </button>
             </div>
           </.inputs_for>
         </div>
 
-        <div class="">
-          <button
+        <div>
+          <.button
             type="button"
             phx-click="add-option"
-            class=""
           >
-            Add another option
-          </button>
+            <.icon name="hero-plus-mini" /> Add option
+          </.button>
+        </div>
 
-          <button class="">
+        <div class="flex justify-end">
+          <.button variant="primary">
             Create Poll
-          </button>
+          </.button>
         </div>
       </.form>
     </div>
